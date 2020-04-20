3e9c4524d9970a938802c8653fcfa9a686956ecd
dusan <dusan@rezdy.com>
2020-04-20 13:58:47 +1000

    generated json2java classes for yahoo client payload

---
+++ src/test/kotlin/com/github/dusanzahoransky/stockanalyst/StockAnalystApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dusanzahoransky.stockanalyst
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockAnalystApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
